783f54154431d40921739ab2b9b6eb73ff7e0fc8
nickgu315 <gulikh@gmail.com>
2021-08-21 18:38:31 +0800

    nickgu.com

--- components/Layout.js
+++ components/Layout.js
@@ -29,6 +29,6 @@ export default function Layout({ title, keywords, description, children }) {
 
 Layout.defaultProps = {
   title: 'NickGu | Passionate about building things!',
-  description: 'Check out my latest project!',
+  description: 'Check out my pass projects!',
   keywords: 'architecture, art, digital art, NFT art, UI/UX',
 }
