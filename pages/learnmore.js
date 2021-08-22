d496be4aacbb097d8c6a17095a16f41ef9a9542a
nickgu315 <gulikh@gmail.com>
2021-08-23 00:24:08 +0800

    design and project obj update3

--- pages/learnmore.js
+++ pages/learnmore.js
@@ -8,7 +8,7 @@ export default function LearnMore() {
       <p>Visualization: Vray, 3dMax, Keyshot</p>
       <p>Coding: React, Node, Python, Flask</p>
 
-      <h2>How do you learn coding/programming as a designer?</h2>
+      <h2>Why did you learn coding/programming as a designer?</h2>
       <p>Besides design, I have always been interested and passionate about coding/ programing. I was first exposed to coding when I  had my exchange semester at the Academy of Fine Arts in Vienna.</p>
       <p>I had an incredible experience there and there was one project that I worked on sparked my interest in coding: Topography Analysis as a way to investigate early formation of Urban City Clusters</p>
 
