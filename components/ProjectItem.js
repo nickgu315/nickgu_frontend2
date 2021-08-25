f0035dccde078fc3a803c00308fb6f6d3b5770c9
nickgu315 <gulikh@gmail.com>
2021-08-26 02:49:35 +0800

    mobile css

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -5,30 +5,30 @@ import styles from '@/styles/ProjectItem.module.css'
 export default function ProjectItem({ evt }) {
   return (
     <div className={styles.project}>
-      <div className={styles.img}>
 
-        <Image
-          src={
-            evt.thumbimage
-              ? evt.thumbimage.formats.small.url
-              : '/images/project-default.png'
-          }
-          width={150}
-          height={150}
-        />
+      <div className={styles.link}>
+        <Link href={`/projects/${evt.slug}`}>
+            <a>
+            <Image className={styles.img}
+              src={
+                evt.thumbimage
+                  ? evt.thumbimage.formats.small.url
+                  : '/images/project-default.png'
+              }
+              width={300}
+              height={300}
+            />
+            </a>
+        </Link>
       </div>
 
       <div className={styles.info}>
 
-        <h3>{evt.name}</h3>
+        <h3 className={styles.projectitemhead}>{evt.name}</h3>
         <h4>{evt.shortd}</h4>
       </div>
 
-      <div className={styles.link}>
-        <Link href={`/projects/${evt.slug}`}>
-          <a className={styles.btn}>Details</a>
-        </Link>
-      </div>
+
     </div>
   )
 }
