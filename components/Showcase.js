43668ba039cd53faeca5a604ad6affe89cd8db3c
nickgu315 <gulikh@gmail.com>
2021-09-02 13:46:12 +0800

    emoji3

--- components/Showcase.js
+++ components/Showcase.js
@@ -10,25 +10,29 @@ export default function Showcase() {
 
       <div className={styles.showcase2}>
         <div className={styles.showcase3}>
-          <div>This site is developed to:</div>
-          <div className={styles.showcase3b}><p>1. Display my pass work as a portfolio site </p> <p className={styles.emo_size}>&nbsp; 🖼️</p></div>
-          <div className={styles.showcase3b}><p>2. Strengthen my coding skills as a Fullstack Developer</p> <p className={styles.emo_size}>&nbsp; 👨‍💻</p></div>
-          <div className={styles.showcase3b}><p>3. Connect to like minded people with similar passion‍</p> <p className={styles.emo_size}>&nbsp;‍ ❤️‍🔥</p></div>
+          <div className={styles.showcase3a}>This site is developed to:</div>
+          <div className={styles.showcase3b}><p>1. Display my pass work as a portfolio site </p> <p className={styles.emo_size0}>&nbsp; 🖼️</p></div>
+          <div className={styles.showcase3b}><p>2. Strengthen my coding skills as a Fullstack Developer</p> <p className={styles.emo_size0}>&nbsp; 👨‍💻</p></div>
+          <div className={styles.showcase3b}><p>3. Connect to like minded people with similar passion‍</p> <p className={styles.emo_size0}>&nbsp;‍ ❤️‍🔥</p></div>
         </div>
-        <h3 className={styles.showcase4}>
-          I co-founded Eone Timepiece Inc. in 2012 <br /> when I was still in grad school and we exited in 2020.
-          <a href="http://www.eone-time.com" target="_blank" rel="noopener noreferrer">www.eone-time.com ⏳</a>
 
-        </h3>
+        <div className={styles.showcase4}>
+          I co-founded Eone Timepiece Inc. in 2012 <br /> when I was still in grad school and we exited in 2020.
+          <a href="http://www.eone-time.com" target="_blank" rel="noopener noreferrer">www.eone-time.com  ⏳</a>
+
+        </div>
       </div>
+
       <div className={styles.showcase2}>
-        <h3 className={styles.showcase3}>My Education: <br />
-        Harvard Graduate School of Design, <br />Master in Architecture (MArchI) <br />-<br />
-        The University of Hong Kong, <br />BA(Architectural Studies)</h3>
-        <h3 className={styles.showcase3}>
+        <div className={styles.showcase3}>
+          <div className={styles.showcase3a}>My Education:</div>
+          <div className={styles.showcase3c}><p>Harvard Graduate School of Design<br />Master in Architecture (MArchI) <span className={styles.emo_size}>👨🏻‍🎓 </span></p></div>
+          <div className={styles.showcase3c}><p>The University of Hong Kong<br />BA(Architectural Studies)<span className={styles.emo_size}>🎓</span></p></div>
+        </div>
+        <div className={styles.showcase4}>
           I am deeply passionate about building products <br />
-          through design and technology <br /> and bring true values to people. {}
-        </h3>
+          through design ✏️ and technology 💡 <br /> and bring true values 📱 🚗 to people. {}
+        </div>
       </div>
     </div>
   )
